072bd257d4a3805b519dd70d944af6790015f8c5
Guillem Barba <guillem@nan-tic.com>
2016-03-03 18:19:31 +0100

    Renamed start_date and end_date to start and end (grafted from a9fd1fedc729d910a54b44c3e01ba5aff27c8d18)

--- working_shift.py
+++ working_shift.py
@@ -3,6 +3,7 @@
 import datetime
 from decimal import Decimal
 
+from trytond import backend
 from trytond.model import Workflow, ModelSQL, ModelView, fields
 from trytond.pyson import Eval
 from trytond.pool import Pool, PoolMeta
@@ -24,18 +25,18 @@ class WorkingShift(Workflow, ModelSQL, ModelView):
     code = fields.Char('Code', readonly=True, required=True)
     employee = fields.Many2One('company.employee', 'Employee', required=True,
         states=STATES, depends=DEPENDS)
-    start_date = fields.DateTime('Start Date', required=True, states=STATES,
+    start = fields.DateTime('Start', required=True, states=STATES,
         depends=DEPENDS)
-    end_date = fields.DateTime('End Date', domain=[
+    end = fields.DateTime('End', domain=[
             ['OR',
-                ('end_date', '=', None),
-                ('end_date', '>', Eval('start_date')),
+                ('end', '=', None),
+                ('end', '>', Eval('start')),
                 ],
             ],
         states={
             'readonly': Eval('state') != 'draft',
             'required': Eval('state').in_(['confirmed', 'done']),
-            }, depends=DEPENDS+['start_date'])
+            }, depends=DEPENDS+['start'])
     hours = fields.Function(fields.Numeric('Hours', digits=(16, 2)),
         'on_change_with_hours')
     interventions = fields.One2Many('working_shift.intervention', 'shift',
@@ -85,23 +86,46 @@ class WorkingShift(Workflow, ModelSQL, ModelView):
                     'because it is not in draft state.'),
                 })
 
+    @classmethod
+    def __register__(cls, module_name):
+        TableHandler = backend.get('TableHandler')
+        cursor = Transaction().cursor
+        sql_table = cls.__table__()
+
+        # Migration from 3.4.0: renamed start/end_date to start/end
+        table = TableHandler(cursor, cls, module_name)
+        start_end_date_exist = (table.column_exist('start_date')
+            and table.column_exist('end_date'))
+
+        super(WorkingShift, cls).__register__(module_name)
+
+        # Migration from 3.4.0: renamed start/end_date to start/end
+        if start_end_date_exist:
+            cursor.execute(*sql_table.update(
+                    columns=[sql_table.start, sql_table.end],
+                    values=[sql_table.start_date, sql_table.end_date]))
+            table = TableHandler(cursor, cls, module_name)
+            table.not_null_action('start', action='add')
+            table.drop_column('start_date')
+            table.drop_column('end_date')
+
     @staticmethod
     def default_state():
         return 'draft'
 
     @staticmethod
-    def default_start_date():
+    def default_start():
         return datetime.datetime.now()
 
     @staticmethod
     def default_employee():
         return Transaction().context.get('employee')
 
-    @fields.depends('start_date', 'end_date')
+    @fields.depends('start', 'end')
     def on_change_with_hours(self, name=None):
-        if not self.start_date or not self.end_date:
+        if not self.start or not self.end:
             return Decimal(0)
-        hours = (self.end_date - self.start_date).total_seconds() / 3600.0
+        hours = (self.end - self.start).total_seconds() / 3600.0
         digits = self.__class__.hours.digits
         return Decimal(str(hours)).quantize(Decimal(str(10 ** -digits[1])))
 
