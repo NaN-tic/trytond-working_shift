b4c7ef49db6271152a9557f8f003c6ea74eb8c6e
Albert Cervera i Areny <albert@nan-tic.com>
2018-10-29 22:26:49 +0100

    Do not use asterisk in imports.

--- __init__.py
+++ __init__.py
@@ -1,15 +1,15 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .configuration import *
-from .intervention import *
-from .working_shift import *
+from . import configuration
+from . import intervention
+from . import working_shift
 
 
 def register():
     Pool.register(
-        WorkingShiftConfiguration,
-        WorkingShiftConfigurationCompany,
-        WorkingShift,
-        Intervention,
+        configuration.WorkingShiftConfiguration,
+        configuration.WorkingShiftConfigurationCompany,
+        working_shift.WorkingShift,
+        intervention.Intervention,
         module='working_shift', type_='model')
