d593352039c695dcdbe8fcf79d87316ad2e00ac9
Carlos G?lvez <carlos.galvez@nan-tic.com>
2019-10-01 09:56:43 +0200

    Upgrade setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
-# This file is part carrier_shipment_note module for Tryton.
+# This file is part working_shift module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 
