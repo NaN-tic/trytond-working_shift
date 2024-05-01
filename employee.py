f22052edbdf48dd92725f2f5c88cb468cbf0b6cf
Albert Cervera i Areny <albert@nan-tic.com>
2024-05-01 13:03:21 +0200

    Add create_working_days field to employee.

--- employee.py
+++ employee.py
@@ -1,6 +1,13 @@
+from trytond.model import fields
 from trytond.pool import PoolMeta
 
 
 class Employee(metaclass=PoolMeta):
     __name__ = 'company.employee'
     _order_name = 'party'
+
+    create_working_days = fields.Boolean('Create Working Days')
+
+    @staticmethod
+    def default_create_working_days():
+        return True
