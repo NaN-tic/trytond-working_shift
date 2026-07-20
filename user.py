ffd7cba696d0fd19f1c3d88a4d0e5d2f2636961a
Raimon Esteve <raimonesteve@gmail.com>
2026-07-20 10:41:42 +0200

    Replace to import trytond.config as config

--- user.py
+++ user.py
@@ -1,8 +1,7 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool, PoolMeta
-from trytond.config import config as config_
-
+import trytond.config as config_
 __all__ = ['User']
 
 LOGIN = config_.getboolean('working_shift', 'login', default=True)
